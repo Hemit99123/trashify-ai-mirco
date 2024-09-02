31776c2ea152acc7b06e3d481c05136109773c02
Hemit Patel <66741904+Hemit99123@users.noreply.github.com>
2024-09-02 19:43:04 -0400

    changed port handling so it can be hosted on any env

--- server/app.py
+++ server/app.py
@@ -59,7 +59,7 @@ class TrashifyAIService(main_pb2_grpc.TrashifyServicer):
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     main_pb2_grpc.add_TrashifyServicer_to_server(TrashifyAIService(), server)
-    server.add_insecure_port("localhost:50051")
+    server.add_insecure_port("[::]:50051")
     server.start()
     server.wait_for_termination()
 
